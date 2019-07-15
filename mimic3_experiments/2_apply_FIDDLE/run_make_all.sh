7bd486dd42c70cf2fa66a186e09b4992c9ea6d29
Shengpu Tang <tangsp@umich.edu>
2019-07-15 12:43:40 -0400

    run FIDDLE

--- mimic3_experiments/2_apply_FIDDLE/run_make_all.sh
+++ mimic3_experiments/2_apply_FIDDLE/run_make_all.sh
@@ -1,39 +1,80 @@
 #!/bin/bash
 set -euxo pipefail
 
-export PYTHONPATH="$PYTHONPATH:../../"
+export PYTHONPATH="../../"
 DATAPATH=$(python -c "import yaml;print(yaml.full_load(open('../config.yaml'))['data_path']);")
+mkdir -p log
 
-
-python prepare_input.py --outcome=ARF --T=4  --dt='1.0'
-# python prepare_input.py --outcome=ARF --T=12 --dt='1.0'
-# python prepare_input.py --outcome=Shock --T=4  --dt='1.0'
-# python prepare_input.py --outcome=Shock --T=12 --dt='1.0'
-
-# python make_features.py --outcome=ARF --T=4  --dt='1.0' > 'log/outcome=ARF.T=4.dt=1.0.out' 2> 'log/outcome=ARF.T=4.dt=1.0.err'
-# python make_features.py --outcome=ARF --T=12 --dt='1.0' > >(tee 'log/outcome=ARF.T=12.dt=1.0.out') 2> >(tee 'log/outcome=ARF.T=12.dt=1.0.err' >&2)
-# python make_features.py --outcome=Shock --T=4  --dt='1.0' > >(tee 'log/outcome=Shock.T=4.dt=1.0.out') 2> >(tee 'log/outcome=Shock.T=4.dt=1.0.err' >&2)
-# python make_features.py --outcome=Shock --T=12 --dt='1.0' > >(tee 'log/outcome=Shock.T=12.dt=1.0.out') 2> >(tee 'log/outcome=Shock.T=12.dt=1.0.err' >&2)
-# python make_features.py \
-#     --data_path='/data4/tangsp/mimic3_features/features/outcome=mortality.T=48.dt=1.0/' \
-#     --population='/data4/tangsp/mimic3_features/population/mortality_48h.csv' \
-#     --T=48 --dt='1.0' \
-#     > >(tee 'log/outcome=mortality.T=48.dt=1.0.out') \
-#     2> >(tee 'log/outcome=mortality.T=48.dt=1.0.err' >&2)
-
-python make_features.py \
-    --data_path='/data4/tangsp/mimic3_features/features/outcome=mortality.T=48.dt=1.0/' \
-    --population='/data4/tangsp/mimic3_features/population/mortality_48h.csv' \
-    --T=48 --dt='1.0'
-    
+OUTCOME=ARF
+T=4.0
+dt=1.0
 python -m FIDDLE.run \
-    --data_path='./small_test/' \
-    --population='./small_test/pop.csv' \
-    --T=4 \
+    --data_path="$DATAPATH/features/outcome=$OUTCOME,T=$T,dt=$dt/" \
+    --population="$DATAPATH/population/${OUTCOME}_${T}h.csv" \
+    --T=$T \
+    --dt=$dt \
+    --theta_1=0.001 \
+    --theta_2=0.001 \
+    --theta_freq=1 \
+    --stats_functions 'min' 'max' 'mean' \
+    > >(tee "log/outcome=$OUTCOME,T=$T,dt=$dt.out") \
+    2> >(tee "log/outcome=$OUTCOME,T=$T,dt=$dt.err" >&2)
+
+OUTCOME=ARF
+T=12.0
+dt=1.0
+python -m FIDDLE.run \
+    --data_path="$DATAPATH/features/outcome=$OUTCOME,T=$T,dt=$dt/" \
+    --population="$DATAPATH/population/${OUTCOME}_${T}h.csv" \
+    --T=$T \
+    --dt=$dt \
+    --theta_1=0.001 \
+    --theta_2=0.001 \
+    --theta_freq=1 \
+    --stats_functions 'min' 'max' 'mean' \
+    > >(tee "log/outcome=$OUTCOME,T=$T,dt=$dt.out") \
+    2> >(tee "log/outcome=$OUTCOME,T=$T,dt=$dt.err" >&2)
+
+OUTCOME=Shock
+T=4.0
+dt=1.0
+python -m FIDDLE.run \
+    --data_path="$DATAPATH/features/outcome=$OUTCOME,T=$T,dt=$dt/" \
+    --population="$DATAPATH/population/${OUTCOME}_${T}h.csv" \
+    --T=$T \
+    --dt=$dt \
+    --theta_1=0.001 \
+    --theta_2=0.001 \
+    --theta_freq=1 \
+    --stats_functions 'min' 'max' 'mean' \
+    > >(tee "log/outcome=$OUTCOME,T=$T,dt=$dt.out") \
+    2> >(tee "log/outcome=$OUTCOME,T=$T,dt=$dt.err" >&2)
+
+OUTCOME=Shock
+T=12.0
+dt=1.0
+python -m FIDDLE.run \
+    --data_path="$DATAPATH/features/outcome=$OUTCOME,T=$T,dt=$dt/" \
+    --population="$DATAPATH/population/${OUTCOME}_${T}h.csv" \
+    --T=$T \
+    --dt=$dt \
+    --theta_1=0.001 \
+    --theta_2=0.001 \
+    --theta_freq=1 \
+    --stats_functions 'min' 'max' 'mean' \
+    > >(tee "log/outcome=$OUTCOME,T=$T,dt=$dt.out") \
+    2> >(tee "log/outcome=$OUTCOME,T=$T,dt=$dt.err" >&2)
+
+
+
+python -m FIDDLE.run \
+    --data_path="$DATAPATH/features/benchmark,outcome=mortality,T=48.0,dt=1.0/" \
+    --population="$DATAPATH/population/pop.mortality_benchmark.csv" \
+    --T=48.0 \
     --dt=1.0 \
     --theta_1=0.001 \
     --theta_2=0.001 \
     --theta_freq=1 \
     --stats_functions 'min' 'max' 'mean' \
-    > >(tee 'log/outcome=mortality.T=48.dt=1.0.out') \
-    2> >(tee 'log/outcome=mortality.T=48.dt=1.0.err' >&2)
+    > >(tee 'log/benchmark,outcome=mortality,T=48.0,dt=1.0.out') \
+    2> >(tee 'log/benchmark,outcome=mortality,T=48.0,dt=1.0.err' >&2)
